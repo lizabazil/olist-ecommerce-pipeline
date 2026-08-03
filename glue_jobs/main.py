72504c35b70dcf16b09911f88826d6f61fea9bf5
Yelyzaveta Tretiak <liza.tretjak@gmail.com>
2026-08-03 17:16:48 +0200

    Detecting invalid data in column order_id

--- glue_jobs/main.py
+++ glue_jobs/main.py
@@ -39,12 +39,14 @@ def inspect_nulls(df):
     print(f"Total number of rows in the df: {rows_number}")
     null_counts_df = df.select([sum((col(c).isNull() | (col(c) == "" if isinstance(df.schema[c].dataType, t.StringType) else col(c).isNull())).cast("int")).alias(c) for c in df.columns])
     null_counts_df.show()
+    return None
 
 def inspecting_int_and_double_columns(list_of_dfs):
     # inspecting only int and double columns
     for curr_df in list_of_dfs:
         num_cols = [f.name for f in curr_df.schema.fields if isinstance(f.dataType, t.IntegerType) or isinstance(f.dataType, t.DoubleType)]
         curr_df.select(num_cols).summary().show()
+    return None
 
 def are_all_timestamps_have_the_same_h_m_s(df, column_name_str):
     # order_reviews 
@@ -53,11 +55,13 @@ def are_all_timestamps_have_the_same_h_m_s(df, column_name_str):
               count(second(f.col(column_name_str)) != 0).alias("non_zero_seconds")
               ).show()
     df.where((hour(f.col(column_name_str)) != 0) | (minute(f.col(column_name_str)) != 0) | (second(f.col(column_name_str)) != 0)).show()
+    return None
 
 def detect_timestamp_pattern_in_string_column(df, column_name):
     detected_timestamp_df = df.where(f.to_timestamp(f.col(column_name), "yyyy-MM-dd HH:mm:ss").isNotNull())
     print(f"Dataframe with rows, where column {column_name} has timestamps data, which indicates invalid data (length={detected_timestamp_df.count()})")
     detected_timestamp_df.show()
+    return None
 
 def detect_invalid_review_id(df, column_name):
     """
@@ -69,6 +73,7 @@ def detect_invalid_review_id(df, column_name):
     if count_of_invalid_rows > 0:
         print(f"Detected rows where column '{column_name}' has invalid id (length={count_of_invalid_rows}): ")
         detected_invalid_id_df.show()
+    return None
 
 
 
@@ -120,11 +125,9 @@ if __name__ == "__main__":
     # detect_timestamp_pattern_in_string_column(order_reviews_df, "review_comment_message")
     order_reviews_df = replace_column_value_to_null(order_reviews_df, "review_comment_message")
 
-    count_before = order_reviews_df.count()
     order_reviews_df = delete_rows_where_review_id_invalid(order_reviews_df, "review_id")
-    count_after = order_reviews_df.count()
-    print(f"Deleted rows={count_before - count_after}")
     #detect_invalid_review_id(order_reviews_df, "review_id")
+    detect_timestamp_pattern_in_string_column(order_reviews_df, "order_id")
 
     job.commit()
         
