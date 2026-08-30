94e27951da797826ede9a094b35533645920ac9a
Yelyzaveta Tretiak <liza.tretjak@gmail.com>
2026-08-30 16:06:25 +0200

    Created file with constants for folder names in S3.

--- sql/athena_constants.py
+++ sql/athena_constants.py
@@ -11,3 +11,8 @@ estimated_vs_actual_delivery_performance = "estimated_vs_actual_delivery_perform
 peak_ordering_hours_and_days = "peak_ordering_hours_and_days"
 category_performance_scorecard = "category_performance_scorecard"
 seller_concentration_risk = "seller_concentration_risk"
+
+all_athena_folder_names = [montly_revenue_and_running_total, payment_method_breakdown_by_revenue, average_order_value_over_time,
+                           revenue_by_installments, customer_lifetime_value_segmentation, customer_acquistion_by_month, 
+                           geographic_customer_distribution, order_status_funnel, estimated_vs_actual_delivery_performance,
+                           peak_ordering_hours_and_days, category_performance_scorecard, seller_concentration_risk]
